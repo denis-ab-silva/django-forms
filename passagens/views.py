86b14263194c320b5ef6a70a4819ce454e645c1d
Denis Silva <denis.ab.silva@hotmail.com>
2021-05-06 21:05:00 -0300

    Incluindo as validações no formulário

--- passagens/views.py
+++ passagens/views.py
@@ -9,5 +9,9 @@ def index(request):
 def minha_consulta(request):
     if request.method == 'POST':
         form = PassagemForms(request.POST)
-        contexto ={'form' : form}
-        return render(request, 'minha_consulta.html', contexto)
+        if form.is_valid():
+            contexto ={'form' : form}
+            return render(request, 'minha_consulta.html', contexto)
+        else:
+            contexto ={'form' : form}
+            return render(request, 'index.html', contexto)
